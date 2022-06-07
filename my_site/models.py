deabff6820b24a51ba0ec53a9aba0e24ae7a1168
Ivan Umpelev <dpakoh.b.kedax@gmail.com>
2022-06-07 08:54:52 +0300

    Semi-commit. Added index.html. Starting creating models.

--- my_site/models.py
+++ my_site/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
+
 # Create your models here.
+class Image(models.Model):
+    image_url = models.URLField()
+
+
+class AbstractProduct(models.Model):
+    class Sex(models.Choices):
+        MEN = 'M', 'Для мужчин'
+        WOMEN = 'W', 'Для женщин'
+        UNISEX = 'U', 'Унисекс'
+        NONE = 'N', 'Не указано'
+
+    product_id = models.IntegerField
+    name = models.CharField(max_length=1024)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    discount = models.IntegerField()
+    images = models.ManyToManyField(ProductImages)
+    sex = models.CharField(max_length=1, choices=Sex.choices, default=Sex.NONE)
+
+    class Meta:
+        abstract = True
+
+
+class Sizes(models.Choices):
+    S = 'S', 'S'
+    M = 'M', 'M'
+    L = 'L', 'L'
+    XL = 'XL', 'XL'
+    XXL = 'XXL', 'XXL'
+
+
+class T_shirt(AbstractProduct):
+    size = models.CharField(max_length='3', choices=Sizes.choices)
--- my_site/views.py
+++ my_site/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 
 # Create your views here.
 
+
 def index(request):
-    return HttpResponse("hello world")
+    template = loader.get_template('my_site/index.html')
+    return HttpResponse(template.render(request=request))
